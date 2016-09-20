4e14cf51fc89637f57a40e4ceddfc19066ea0773
Rey Jenald Pena <reyjenaldpena@gmail.com>
2016-09-20 21:32:48 +0900

    add Pending VC prerequisites

---
+++ TodoAppML/Model/MLPendingVCModel.swift
@@ -0,0 +1,13 @@
+//
+//  NLPendingVCModel.swift
+//  TodoAppML
+//
+//  Created by Rey Pena on 20/09/2016.
+//  Copyright © 2016 Malaysia. All rights reserved.
+//
+
+import UIKit
+
+class MLPendingVCModel: NSObject {
+
+}
---
+++ TodoAppML/View/MLPendingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MLPendingTableViewCell.swift
+//  TodoAppML
+//
+//  Created by Rey Pena on 20/09/2016.
+//  Copyright © 2016 Malaysia. All rights reserved.
+//
+
+import UIKit
+
+class MLPendingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
