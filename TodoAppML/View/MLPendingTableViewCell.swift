fd9c102ae40bc3a052daa88e1bdcff331788d64d
Rey Jenald Pena <reyjenaldpena@gmail.com>
2016-09-20 23:25:42 +0900

    create pending cell

--- TodoAppML/View/MLPendingTableViewCell.swift
+++ TodoAppML/View/MLPendingTableViewCell.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import Foundation
 
 class MLPendingTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    let DEFAULT_COUNT = Int(5)
+    
+    weak var delegate : MLPendingTableViewCellDelegate?
+    
+    private var timer : NSTimer!
+    private var countDown : Int = 5
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +30,67 @@ class MLPendingTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    // MARK: Private Functions
+    
+    func updateCounter() {
+        self.countDown = self.countDown - 1
+        if self.countDown == 0 {
+            if let delegate = self.delegate {
+                self.countDown = DEFAULT_COUNT
+                self.startCancelAnimation(false)
+                delegate.pendingCellDidFinishCount(self)
+                self.timer.invalidate()
+            }
+        }
+    }
+    
+    func startCancelAnimation(start : Bool) {
+        
+        if start {
+            let pulseAnimation = CABasicAnimation(keyPath: "opacity")
+            pulseAnimation.duration = 1.25
+            pulseAnimation.fromValue = 0.6
+            pulseAnimation.toValue = 1
+            pulseAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+            pulseAnimation.autoreverses = true
+            pulseAnimation.repeatCount = FLT_MAX
+            self.cancelButton.layer.backgroundColor = UIColor.redColor().CGColor
+            self.cancelButton.layer.addAnimation(pulseAnimation, forKey: "animateOpacity")
+        } else {
+            self.cancelButton.hidden = true
+            self.timer.invalidate()
+            self.countDown = DEFAULT_COUNT
+            self.cancelButton.layer.removeAllAnimations()
+        }
+    }
+    
+    // MARK: Pubic Functions
+    
+    func animateCancelButton() {
+        
+        self.cancelButton.hidden = !self.cancelButton.hidden
+        
+        if self.cancelButton.hidden {
+            self.timer.invalidate()
+            self.startCancelAnimation(false)
+        } else {
+            self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+            self.startCancelAnimation(true)
+        }
+    }
+    
+    // MARK: IBActions
 
+    @IBAction func didTapCancelButton(sender: AnyObject) {
+        self.countDown = DEFAULT_COUNT
+        if let delegate = self.delegate {
+            delegate.pendingCellDidCancelOperation(self)
+        }
+    }
+}
+
+@objc protocol MLPendingTableViewCellDelegate : class {
+    func pendingCellDidFinishCount(cell : MLPendingTableViewCell)
+    func pendingCellDidCancelOperation(cell : MLPendingTableViewCell)
 }
